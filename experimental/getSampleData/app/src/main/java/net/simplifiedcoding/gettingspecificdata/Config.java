705e6bf59b26a7532d448398c883ad80d6b4e3fd
andy campbell <D00159594@student.dkit.ie>
2017-05-01 21:23:07 +0100

    retrieve data from mysql databases annd parse into android studio app

---
+++ experimental/getSampleData/app/src/main/java/net/simplifiedcoding/gettingspecificdata/Config.java
@@ -0,0 +1,13 @@
+package net.simplifiedcoding.gettingspecificdata;
+
+/**
+ * Created by Belal on 10/12/2015.
+ */
+public class Config {
+    public static final String DATA_URL = "http://192.168.94.1/Android/College/getData.php?id=";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_ADDRESS = "address";
+    public static final String KEY_VC = "vc";
+    public static final String JSON_ARRAY = "result";
+
+}
